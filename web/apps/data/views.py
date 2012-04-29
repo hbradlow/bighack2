6626ce2f5e235ef2b19dd75e4bfcf44a76be00a5
Henry Bradlow <hbradlow@berkeley.edu>
2012-04-29 02:43:22 -0700

    Added a tab for pie chart

--- web/apps/data/views.py
+++ web/apps/data/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from  django.shortcuts import *
 from django.core.urlresolvers import reverse
 
+from BeautifulSoup import BeautifulSoup
+import mechanize 
+import urllib
+
 def tmp(request):
 	appliances = [] 
 	appliances.append("Fridge")
@@ -12,3 +16,15 @@ def tmp(request):
 	user_data = [5000,4000,6000]
 	average_data = [4500,4500,7000]
 	return render_to_response("main.html",{"appliances":appliances,"user_data":user_data,"average_data":average_data},context_instance=RequestContext(request))
+
+def collect(request):
+    unit_type = request.GET["unit_type"]
+    unit_nr = request.GET["unit_nr"]
+
+    br = mechanize.Browser()
+    br.open("http://amazon.com") 
+    br.select_form(name="site-search") 
+    br['field-keywords'] = unit_type + " " + unit_nr
+    response = br.submit()
+    soup = BeautifulSoup(response.read())
+    return HttpResponse(str(soup.find("div", {"id" : "result_0"})))
