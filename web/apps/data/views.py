2931245f3d853680fe672cf59d2bb57372b46807
Henry Bradlow <hbradlow@berkeley.edu>
2012-04-29 03:26:12 -0700

    Working on charts

--- web/apps/data/views.py
+++ web/apps/data/views.py
@@ -13,8 +13,8 @@ def tmp(request):
 	appliances.append("Dish Washer")
 	appliances.append("Toaster")
 
-	user_data = [5000,4000,6000]
-	average_data = [4500,4500,7000]
+	user_data = [5000,4000,6000,1000]
+	average_data = [4500,4500,7000,1000]
 	return render_to_response("main.html",{"appliances":appliances,"user_data":user_data,"average_data":average_data},context_instance=RequestContext(request))
 
 def collect(request):
